8325be133c8e4f8ee86dc5f4955a072c02ff57da
yuhao <yuhao@zhiketong.cn>
2021-01-07 15:34:55 +0800

    set request required

--- src/templates/config.ts
+++ src/templates/config.ts
@@ -3,7 +3,7 @@ interface IConfig {
      * Make a HTTP request operation.
      * @param options The parameters required by the request.
      */
-    request?(options: {
+    request(options: {
         url: string,
         method: string,
         query?: Record<string, any>,
