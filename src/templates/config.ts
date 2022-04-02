5924f520376b78b28e69b567510fb42220ec9aa3
yuhao <yuhao@zhiketong.cn>
2022-04-02 18:58:35 +0800

    support globalThis

--- src/templates/config.ts
+++ src/templates/config.ts
@@ -15,21 +15,34 @@ interface IConfig {
 
 const GLOBAL_CONFIG_KEY = "__SWAGGER_CONFIG_REQUEST__";
 
+const global = (function () {
+    if (typeof globalThis !== "undefined") {
+        return globalThis;
+    }
+    if (typeof self !== "undefined") {
+        return self;
+    }
+    if (typeof window !== "undefined") {
+        return window;
+    }
+    throw new Error("unable to locate global object");
+}());
+
 const defaultConfig = {
     _request: null as any,
     get request(): (options: IRequestOptions) => Promise<any> {
         if (this._request) {
             return this._request;
-        } else if (globalThis && GLOBAL_CONFIG_KEY in globalThis) {
-            return globalThis[GLOBAL_CONFIG_KEY];
+        } else if (global && GLOBAL_CONFIG_KEY in global) {
+            return global[GLOBAL_CONFIG_KEY];
         } else {
             return () => Promise.reject('The [swagger-api#request] option is required') as Promise<any>;
         }
     },
     set request(value: (options: IRequestOptions) => Promise<any>) {
         this._request = value;
-        if (globalThis) {
-            globalThis[GLOBAL_CONFIG_KEY] = value;
+        if (global) {
+            global[GLOBAL_CONFIG_KEY] = value;
         }
     }
 }
